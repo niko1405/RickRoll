12d2a6cf0b8496037db0d19099b94d8587359539
niko1405 <107156262+niko1405@users.noreply.github.com>
2023-10-16 01:03:29 -0400

    final commit

---
+++ src/components/SplineScene.tsx
@@ -0,0 +1,11 @@
+import Spline from "@splinetool/react-spline";
+
+const SplineScene = () => {
+  return (
+    <div className="w-screen h-screen bg-gray-800 flex flex-col items-center justify-center">
+      <Spline scene="https://prod.spline.design/dC15IrwajPWUjopo/scene.splinecode" />
+    </div>
+  );
+};
+
+export default SplineScene;
