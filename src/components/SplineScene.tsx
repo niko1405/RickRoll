9a8e4234c1dff5745f01b906bc53fc854cb67ac0
niko1405 <107156262+niko1405@users.noreply.github.com>
2023-10-16 23:17:10 -0400

    fix orientation

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -1,6 +1,27 @@
 import Spline from "@splinetool/react-spline";
+import { useEffect, useState } from "react";
 
 const SplineScene = () => {
+  const [landscape, setLandscape] = useState<boolean>(false);
+
+  useEffect(() => {
+    let portrait = window.matchMedia("(orientation: portrait)");
+
+    let handleOrientation = (e: any) => {
+      if (e.matches) {
+        // Portrait mode
+        console.log("portrait");
+      } else {
+        // Landscape
+        console.log("landscape");
+      }
+    };
+
+    portrait.addEventListener("change", handleOrientation);
+
+    return () => portrait.removeEventListener("change", handleOrientation);
+  }, []);
+
   return (
     <div className="w-screen h-screen bg-gray-800 flex flex-col items-center justify-center">
       <Spline scene="https://prod.spline.design/dC15IrwajPWUjopo/scene.splinecode" />
