f527868166bd32737154d6f96ec6a7d245d1166e
niko1405 <107156262+niko1405@users.noreply.github.com>
2023-10-16 23:24:16 -0400

    fix orientation

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -2,27 +2,27 @@ import Spline from "@splinetool/react-spline";
 import { useEffect, useState } from "react";
 
 const SplineScene = () => {
-  const [landscape, setLandscape] = useState<boolean>(false);
+  //   const [landscape, setLandscape] = useState<boolean>(false);
 
-  useEffect(() => {
-    let portrait = window.matchMedia("(orientation: portrait)");
+  //   useEffect(() => {
+  //     let portrait = window.matchMedia("(orientation: portrait)");
 
-    let handleOrientation = (e: any) => {
-      if (e.matches) {
-        // Portrait mode
-        console.log("portrait");
-        setLandscape(false);
-      } else {
-        // Landscape
-        console.log("landscape");
-        setLandscape(true);
-      }
-    };
-    portrait.addEventListener("change", handleOrientation);
+  //     let handleOrientation = (e: any) => {
+  //       if (e.matches) {
+  //         // Portrait mode
+  //         console.log("portrait");
+  //         setLandscape(false);
+  //       } else {
+  //         // Landscape
+  //         console.log("landscape");
+  //         setLandscape(true);
+  //       }
+  //     };
+  //     portrait.addEventListener("change", handleOrientation);
 
-    return () => portrait.removeEventListener("change", handleOrientation);
-  }, []);
-  console.log("landscape: " + landscape);
+  //     return () => portrait.removeEventListener("change", handleOrientation);
+  //   }, []);
+  //   console.log("landscape: " + landscape);
 
   return (
     <div className="w-screen h-screen bg-gray-800 flex flex-col items-center justify-center">
