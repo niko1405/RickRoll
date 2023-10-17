c9dc8f69d54f55b933f533adae2a4a8aeeaf3b12
niko1405 <107156262+niko1405@users.noreply.github.com>
2023-10-16 23:28:04 -0400

    fix orientation

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -1,5 +1,4 @@
 import Spline from "@splinetool/react-spline";
-import { useEffect, useState } from "react";
 
 const SplineScene = () => {
   //   const [landscape, setLandscape] = useState<boolean>(false);
